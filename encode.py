6353ba949531b60e6e24fc6e2a97b5293250b1b5
jwb <jared.briskman@students.olin.edu>
2017-12-06 21:34:15 -0500

    Getting a circle in vis

--- encode.py
+++ encode.py
@@ -9,7 +9,7 @@ def encode(arr1, arr2, fname):
 
 if __name__ == '__main__':
     fname = 'send.dat'
-    tmp = np.append(np.repeat(1, 5000), np.zeros(5000))
-    arr1 = np.repeat(tmp, 4)
+    tmp = np.append(np.repeat(1, 1000), np.zeros(1000))
+    arr1 = np.repeat(tmp, 6)
     arr2 = np.zeros(len(arr1))
     encode(arr1, arr2, fname)
