c8d32e467b3483e8d069b370ed336bb8035c5bd4
kailevy <kai0levy@gmail.com>
2017-12-07 12:36:29 -0500

    Update encode for smaller pulses, qam

--- encode.py
+++ encode.py
@@ -10,7 +10,13 @@ def encode(sig, fname):
 
 if __name__ == '__main__':
     fname = 'send.dat'
-    tmp = np.append(np.repeat(1, 1000), np.repeat(-1, 1000))
+    # Send ascii 'a' 0b01100001 and '!' 0b00100001
+    # not really but 0b01110001 and 0b00100101
+    pp = np.repeat(1+1j, 100)
+    mm = np.repeat(-1-1j, 100)
+    pm = np.repeat(1-1j, 100)
+    mp = np.repeat(-1+1j, 100)
+    tmp = np.concatenate((mm, pm, pp, pm, mm, mp, mm, pp))
     arr1 = np.tile(tmp, 20)
     arr1 = np.insert(arr1, 0, np.zeros(5000))
     arr1 = np.append(arr1, np.zeros(5000))
