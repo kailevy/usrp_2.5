d76040d36aaa652e31cd95a46a2543d9dbd62e49
kailevy <kai0levy@gmail.com>
2017-12-07 10:57:47 -0500

    Update encode, fix bugs

--- encode.py
+++ encode.py
@@ -1,17 +1,17 @@
 import numpy as np
+# import matplotlib.pyplot as plt
 
-def encode(arr1, arr2, fname):
-    np.savetxt(fname, np.column_stack([
-        arr1.view(float).reshape(-1, 2),
-        arr2.view(float).reshape(-1, 2),
-    ]))
+def encode(sig, fname):
+    tmp = np.zeros(2*len(sig), dtype='float32')
+    tmp[::2] = np.real(sig)
+    tmp[1::2] = np.imag(sig)
+    tmp.tofile(fname)
 
 
 if __name__ == '__main__':
     fname = 'send.dat'
-    tmp = np.append(np.repeat(1, 5000), np.repeat(-1, 5000))
-    arr1 = np.repeat(tmp, 4)
+    tmp = np.append(np.repeat(1, 1000), np.repeat(-1, 1000))
+    arr1 = np.tile(tmp, 20)
     arr1 = np.insert(arr1, 0, np.zeros(5000))
     arr1 = np.append(arr1, np.zeros(5000))
-    arr2 = np.zeros(len(arr1))
-    encode(arr1, arr2, fname)
+    encode(arr1, fname)
