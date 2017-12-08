fa447ce1506ffcdedccd1b8769e0250d04c51fbe
kailevy <kai0levy@gmail.com>
2017-12-08 16:36:35 -0500

    Function-ize the pulse maker

--- encode.py
+++ encode.py
@@ -1,6 +1,17 @@
 import numpy as np
 # import matplotlib.pyplot as plt
 
+PP = np.repeat(1+1j, 100)
+MM = np.repeat(-1-1j, 100)
+PM = np.repeat(1-1j, 100)
+MP = np.repeat(-1+1j, 100)
+BITS = np.array([MM, MP, PM, PP])
+
+def make_pulse(data):
+    signal = BITS[data]
+    signal = signal.flatten()
+    return signal
+
 def encode(sig, fname):
     tmp = np.zeros(2*len(sig), dtype='float32')
     tmp[::2] = np.real(sig)
@@ -14,14 +25,8 @@ if __name__ == '__main__':
     # not really but 0b01110001 and 0b00100101
     header = np.array([3, 0, 0, 3, 3, 0, 2, 0, 2, 3, 3, 1, 2, 3, 1, 1, 1, 3, 3, 3, 2, 2, 3,
        1, 2, 0, 1, 0, 2, 2, 1, 1])
-    pp = np.repeat(1+1j, 100)
-    mm = np.repeat(-1-1j, 100)
-    pm = np.repeat(1-1j, 100)
-    mp = np.repeat(-1+1j, 100)
-    bits = np.array([mm, mp, pm, pp])
-    header = bits[header]
-    header = header.flatten()
-    tmp = np.concatenate((mm, pm, pp, pm, mm, mp, mm, pp))
+    header = make_pulse(header)
+    tmp = make_pulse(np.array([1,2,3,2,0,1,0,3]))
     arr1 = np.tile(tmp, 20)
     arr1 = np.concatenate((np.zeros(5000), header, np.zeros(1000), arr1))
     arr1 = np.append(arr1, np.zeros(5000))
