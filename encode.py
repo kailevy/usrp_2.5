a0f4906028808fbfd299b773dd1033bd312b312b
Alexander Hoppe <alexanderehoppe@gmail.com>
2017-12-12 23:29:04 -0500

    Add byte list transmitting code

--- encode.py
+++ encode.py
@@ -18,6 +18,15 @@ def encode_string(input_string):
             quad_arr.append( (ord(char) >> 2 * i) & 0x03 )
     return quad_arr
 
+# Take in a list of bytes and return a list of MSB-first QAM values
+def encode_byte_list(input_list):
+    quad_arr = []
+    for byte in input_string:
+        for i in [3,2,1,0]:
+            # Right shift by the right amount and mask off last 2 bits
+            quad_arr.append( byte >> 2 * i) & 0x03 )
+    return quad_arr
+
 # Take an array of 0-3 QAM values and reconstruct a string
 def decode_string(quad_arr):
     if len(quad_arr) % 4 != 0: return 'ERR'
@@ -32,6 +41,21 @@ def decode_string(quad_arr):
             output_string += (chr(curr_ord))
             curr_ord = 0
     return output_string
+
+# Take an array of 0-3 MSB-first QAM values and reconstruct a byte list
+def decode_byte_list(quad_arr):
+    if len(quad_arr) % 4 != 0: return 'ERR'
+    idx = 0
+    curr_byte = 0
+    output_list = []
+    shift = 0
+    for symbol in quad_arr:
+        curr_byte += symbol << (3-shift)*2
+        shift = (shift + 1) % 4
+        if shift == 0:
+            output_list.append(curr_byte)
+            curr_byte = 0
+    return output_list
 
 def generate_white_noise(std=5, samples=5000, seed=4):
     mean = 0
@@ -66,20 +90,19 @@ if __name__ == '__main__':
     noise_footer5 = generate_white_noise(seed=5)
 
 
-
-    tx = encode_string('abcdefghijklmnopqrstuvwxyz')
-    tx_data = make_pulse(tx)
-    arr1 = np.concatenate((np.zeros(50*pulse_length), noise_header4, header_pulse, tx_data, noise_footer5))
-    signal = encode(arr1, fname)
-
-    expected = header + tx
-    # print expected
-    # print len(expected)
-    # print('num_samples:' + str(len(arr1)/pulse_length))
-
     with open('gb_addr.txt', 'r') as infile:
-        gb_addr = ''.join(infile.readlines())
+        tx = encode_string(''.join(infile.readlines()))
+        tx_data = make_pulse(tx)
+        arr1 = np.concatenate((np.zeros(50*pulse_length), noise_header4, header_pulse, tx_data, noise_footer5))
+        signal = encode(arr1, fname)
 
-        print(gb_addr)
-        print(encode_string(gb_addr))
-        print(decode_string(encode_string(gb_addr)))
+        expected = header + tx
+        print(expected)
+        print(len(expected))
+        print('num_samples:' + str(len(arr1)/pulse_length))
+
+    #     gb_addr = ''.join(infile.readlines())
+    #
+    #     print(gb_addr)
+    #     print(encode_string(gb_addr))
+    #     print(decode_string(encode_string(gb_addr)))
